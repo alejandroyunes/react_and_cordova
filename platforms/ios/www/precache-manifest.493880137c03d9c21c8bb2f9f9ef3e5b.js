b993464a48ead3dfd2fe5a9addf69a9fa77364d6
Alejandro Yunes <alejandroyunes@outlook.com>
2019-09-14 13:18:39 -0500

    both platforms added and npm install acorn

---
+++ platforms/ios/www/precache-manifest.493880137c03d9c21c8bb2f9f9ef3e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bee7feca594e0ed5c652f801892150",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bb3ba9b2a272bacc3ef",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "40d963ad898ed3c370f5",
+    "url": "./static/css/main.363bc90d.chunk.css"
+  },
+  {
+    "revision": "8bb3ba9b2a272bacc3ef",
+    "url": "./static/js/2.469cadb2.chunk.js"
+  },
+  {
+    "revision": "40d963ad898ed3c370f5",
+    "url": "./static/js/main.43bc2ba3.chunk.js"
+  },
+  {
+    "revision": "92b7010270b0ac324f72",
+    "url": "./static/js/runtime~main.dd97c86a.js"
+  }
+]);
